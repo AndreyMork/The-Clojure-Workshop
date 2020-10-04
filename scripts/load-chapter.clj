d76e9c44988a61d6d352f37b6f74423215b78c35
Andrey Mork <35698586+AndreyMork@users.noreply.github.com>
2020-10-04 13:55:54 +0300

    feat: add "load-chapter" script

---
+++ scripts/load-chapter.clj
@@ -0,0 +1,8 @@
+(defn make-chapter
+  [chapter-name]
+  (fn [filename]
+    (let [fullname (str chapter-name "/" filename)]
+      (load-file fullname))))
+
+(def chapter1 (make-chapter "01.Hello-REPL"))
+(def chapter2 (make-chapter "02.Data-Types-and-Immutability"))
