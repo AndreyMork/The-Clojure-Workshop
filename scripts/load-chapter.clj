9cb62929f8bf7aa8caf8a30a2437f7a84cbeb828
Andrey Mork <35698586+AndreyMork@users.noreply.github.com>
2021-02-09 03:00:16 +0300

    feat: update `load-chapter` script

--- scripts/load-chapter.clj
+++ scripts/load-chapter.clj
@@ -1,9 +1,26 @@
-(defn make-chapter
-  [chapter-name]
-  (fn [filename]
-    (let [fullname (str chapter-name "/" filename)]
-      (load-file fullname))))
+(def chapters
+  {1 "01.Hello-REPL"
+   2 "02.Data-Types-and-Immutability"
+   3 "03.Functions-in-Depth"
+   4 "04.Mapping-and-Filtering"})
 
-(def chapter1 (make-chapter "01.Hello-REPL"))
-(def chapter2 (make-chapter "02.Data-Types-and-Immutability"))
-(def chapter3 (make-chapter "03.Functions-in-Depth"))
+(def extension ".clj")
+
+(defn pad-number
+  [n]
+  (if (< n 10) (str "0" n) (str n)))
+
+(defn build-activity-filename
+  [chapter-number activity-number]
+  (str "activity-" chapter-number "." (pad-number activity-number) extension))
+
+(def build-filename-fns
+  {:activity build-activity-filename})
+
+(defn build-chapter-path
+  [chapter-number file-type file-number]
+  (let [chapter-name (chapters chapter-number)
+        filename ((build-filename-fns file-type) chapter-number file-number)]
+    (clojure.string/join "/" [chapter-name filename])))
+
+(def chapter (comp load-file build-chapter-path))
